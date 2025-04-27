f28e64bfa9914341e7bedf90f7f6012b458b2fa1
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2025-04-27 16:52:26 +0100

    Add PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,55 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+    registerType: "prompt",
+    manifest: {
+        name: "Robinson",
+        short_name: "Robinson",
+        description: "Your personal library manager",
+        icons: [
+            {
+                src: "/web-app-manifest-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "favicon",
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "favicon",
+            },
+            {
+                src: "/apple-touch-icon.png",
+                sizes: "180x180",
+                type: "image/png",
+                purpose: "apple touch icon",
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any maskable",
+            },
+        ],
+        theme_color: "#1976d2",
+        background_color: "#1976d2",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+    },
+};
 
 export default () => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
     return defineConfig({
-        plugins: [react()],
+        plugins: [react(), VitePWA(manifestForPlugIn)],
         server: {
             port: 3000,
             host: true,
