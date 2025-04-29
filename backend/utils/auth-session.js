f148add5a262a71a00c48b3387d740cc3497a111
Ryan McCartney <33418788+ryanmccartney@users.noreply.github.com>
2025-04-29 08:54:25 +0100

    Add explicit secure environment variable for session store

--- backend/utils/auth-session.js
+++ backend/utils/auth-session.js
@@ -12,6 +12,7 @@ const dbPassword = process.env.DB_PASSWORD || "robinson123";
 
 const uri = `mongodb://${dbUser}:${dbPassword.replace("@", "%40")}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
 const sessionSecret = process.env.SESSION_SECRET || "sup3rs3cr3t";
+const sessionSecure = Boolean(process.env.SESSION_SECURE) || false;
 
 const store = new MongoDBStore({
     uri: uri,
@@ -23,7 +24,7 @@ const mongoSession = () => {
         secret: sessionSecret,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-            secure: nodeEnv === "development" ? false : true,
+            secure: sessionSecure,
         },
         store: store,
         resave: false,
