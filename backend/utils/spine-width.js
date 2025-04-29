0c525ab42898f69a2e4e6412836f67bad4c10478
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2025-04-29 19:02:11 +0100

    Update spine width calculation

--- backend/utils/spine-width.js
+++ backend/utils/spine-width.js
@@ -1,9 +1,10 @@
-const pageWidth = process.env.PAGE_WIDTH || 0.009;
-const hardbackCoverWidth = process.env.HARDBACK_COVER_WIDTH || 0.5;
-const paperbackCoverWidth = process.env.PAPERBACK_COVER_WIDTH || 0.2;
+const pageWidth = parseFloat(process.env.PAGE_WIDTH) || 0.07;
+const hardbackCoverWidth = parseFloat(process.env.HARDBACK_COVER_WIDTH) || 0.5;
+const paperbackCoverWidth =
+    parseFloat(process.env.PAPERBACK_COVER_WIDTH) || 0.2;
 
 module.exports = (pages, hardback = false) => {
     const adjustedPages = pages ? pages : 275;
     const coverWidth = hardback ? hardbackCoverWidth : paperbackCoverWidth;
-    return adjustedPages * pageWidth + coverWidth;
+    return (adjustedPages / 2) * pageWidth + coverWidth;
 };
